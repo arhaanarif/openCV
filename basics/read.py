9eb118d6db12f06e51470a6c206c4e315822519a
arhaanarif <arhaanarifsaifi@gmail.com>
2024-10-30 12:12:47 +0530

    updates'

--- basics/read.py
+++ basics/read.py
@@ -3,4 +3,6 @@ import cv2 as cv
 
 img=cv.imread('C:/SDE/openCV/Data/images/1000.jpg')
 cv.imshow("1MVA",img)
-cv.waitKey(0)
+cv.waitKey(10000)       #image wait time (ms) 0-stay forever
+cv.destroyAllWindows()  #closes all windows
+cv.destroyWindow()      #closes one window
